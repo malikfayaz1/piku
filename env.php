62517d2730ff943a6c13f1ff007a64d7a026ae2d
malikfayaz1 <infomaliks7@gmail.com>
2021-02-16 16:47:08 +0500

    Add files via upload

---
+++ env.php
@@ -0,0 +1,10 @@
+<?php
+
+define('CF_BASE_URL', 'http://localhost/the-script12');
+define('CF_DB_HOST', 'localhost');
+define('CF_DB_NAME', 'database12142a');
+define('CF_DB_USER', 'root');
+define('CF_DB_PASSWORD', '123456');
+define('CF_DB_PREFIX', 'cf_');
+define('CF_DB_TYPE', 'mysqli');
+define('CF_DEMO', false);
